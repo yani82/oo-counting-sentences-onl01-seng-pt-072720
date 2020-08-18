d92356357a85d3f0a6acc53f95e0fed1fb632b0e
Yani Asari <yani.iman1@gmail.com>
2020-08-18 04:30:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.?!]/) 
+    sentence_array.delete_if{|sentence| sentence.empty?}
+    sentence_array.length 
   end
 end
